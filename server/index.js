ca73355b164948ca5f36e230228816f322dee031
Xiaocong Lau <12184795@qq.com>
2020-05-21 14:57:18 +0800

    SSR ignore less style

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 require('@babel/register')()
 
 require('@babel/polyfill')
+require.extensions['.less'] = () => {
+  return;
+};
+require.extensions['.scss'] = () => {
+  return;
+};
+require.extensions['.css'] = () => {
+  return;
+};
+require.extensions['.styl'] = () => {
+  return;
+};
 require('./server')
--- server/server.js
+++ server/server.js
@@ -1,22 +1,52 @@
+import { renderToString } from 'react-dom/server'
 import path from 'path';
 import Express from 'express';
 import React from 'react';
+import App from '../src/App';
+import { Provider } from 'react-redux';
+import { StoreContext } from 'redux-react-hook';
+import store from '../src/store';
 
 const app = Express();
-// 每当收到请求时都会触发
-//app.use(handleRender);
-//
-//// 接下来会补充这部分代码
-//function handleRender(req, res) {
-//}
-//
-//function renderFullPage(html, preloadedState) {
-//  /* ... */
-//}
+app.use(handleRender);
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
-});
+function renderFullPage(html, preloadedState) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Redux Universal Example</title>
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>
+          // 警告：关于在 HTML 中嵌入 JSON 的安全问题，请查看以下文档
+          // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+    /</g,
+    '\\u003c'
+  )}
+        </script>
+        <script src="/static/bundle.js"></script>
+      </body>
+    </html>
+    `
+}
+
+function handleRender(req, res) {
+  //const html = renderToString(
+  //  <React.StrictMode>
+  //    <Provider store={store}>
+  //      <StoreContext.Provider value={ store }>
+  //        <App />
+  //      </StoreContext.Provider>
+  //    </Provider>
+  //  </React.StrictMode>,
+  //);
+  const html = renderToString(<App />);
+  const preloadedState = store.getState()
+  res.send(renderFullPage(html, preloadedState));
+}
 
 var server = app.listen(8081, function() {
 
